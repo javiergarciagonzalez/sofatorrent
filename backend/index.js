8a0ed4a82973d8c10be7f234ff43ae5b0f8aae4b
Javier García González <javier@garciagon.com>
2019-10-06 13:46:09 +0200

    Remove commented code

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,8 @@
-const path = require('path');
 const express = require('express');
 
 const app = express();
 
 const port = process.env.PORT || 8080;
-
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public', 'index.html'));
-// });
 
 app.use(express.static('dist'));
 
