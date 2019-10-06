3179aeeb2c3569a96af57cccfe030b41c799e76c
Javier García González <javier@garciagon.com>
2019-10-06 13:20:59 +0200

    Add eslint + babel + prettier config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-    plugins: [
-        '@babel/plugin-proposal-class-properties'
-    ],
+    plugins: ['@babel/plugin-proposal-class-properties'],
     presets: ['@babel/react', ['@babel/env']]
 };
