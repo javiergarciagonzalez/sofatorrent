4275bea9c7e5f861d9f6052c09eedacebc00b97d
Javier García González <javier@garciagon.com>
2019-10-13 09:10:04 +0200

    Add first iteration with rarbg API

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,26 @@
 /* eslint-disable no-console */
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const rarbgApi = require('rarbg-api');
 
 const app = express();
 
 const port = process.env.PORT || 8080;
 
 app.use(express.static('dist'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
 });
 
-app.post('/api/search', (req, res) => {
-    console.log(req.params);
+const fetchRarbgSearchResults = async (req, res) => {
+    const keyword = req.body.search;
 
-    res.send('hola');
-});
+    const results = await rarbgApi.search(keyword);
+
+    res.send(results);
+};
+
+app.post('/api/search', fetchRarbgSearchResults);
