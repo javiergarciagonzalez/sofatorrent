f1656d220443d5de8548daa0d4d3ae9bb4e98bbb
Javier García González <javier@garciagon.com>
2019-10-20 10:28:04 +0200

    Add sass to project

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const autoPrefixer = require('autoprefixer');
+
+module.exports = {
+    plugins: [autoPrefixer]
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const sass = require('sass');
 
 const outputDirectory = 'public/dist';
 
@@ -20,6 +22,32 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                            importLoaders: 2
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: sass,
+                            sourceMap: true
+                        }
+                    }
+                ]
             },
             {
                 test: /\.css$/,
@@ -42,6 +70,9 @@ module.exports = {
     },
     plugins: [
         new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
         new HtmlWebpackPlugin({
             template: './public/index.html',
             favicon: './public/favicon.ico'
