5d2419247f9cf1b36946cba6e6058376accf6b37
Javier García González <javier@garciagon.com>
2019-10-21 08:39:22 +0200

    Replace math.pow by exponential operator

--- backend/controllers/rarbgApi.js
+++ backend/controllers/rarbgApi.js
@@ -4,7 +4,7 @@ const bytesToSize = bytes => {
     const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
     if (bytes === 0) return '0 Byte';
     const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
-    return `${Math.round(bytes / Math.pow(1024, i), 2)} ${sizes[i]}`;
+    return `${Math.round(bytes / 1024 ** i, 2)} ${sizes[i]}`;
 };
 
 const fetchRarbgSearchResults = async (req, res) => {
