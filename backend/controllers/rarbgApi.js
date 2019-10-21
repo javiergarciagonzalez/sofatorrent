9de03ede136131be71df61cce246cc71d7c3d509
Javier García González <javier@garciagon.com>
2019-10-21 08:37:28 +0200

    Normalize torrent size info

--- backend/controllers/rarbgApi.js
+++ backend/controllers/rarbgApi.js
@@ -1,5 +1,12 @@
 const rarbgApi = require('rarbg-api');
 
+const bytesToSize = bytes => {
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes === 0) return '0 Byte';
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
+    return `${Math.round(bytes / Math.pow(1024, i), 2)} ${sizes[i]}`;
+};
+
 const fetchRarbgSearchResults = async (req, res) => {
     const keyword = req.body.search;
 
@@ -10,7 +17,7 @@ const fetchRarbgSearchResults = async (req, res) => {
         return {
             title,
             link,
-            size
+            size: bytesToSize(size)
         };
     });
 
